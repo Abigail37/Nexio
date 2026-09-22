147f87217bd5ea64e883145bdf8050b1a398e63c
Abigail Ogunmola <abigailogunmola37@gmail.com>
2026-09-22 16:15:51 +0100

    Remove sensitive config files from repository

--- includes/paystack_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('PAYSTACK_PUBLIC_KEY', 'pk_test_33d0d01b0bf7a547d3e64757fe732a0364773932');
-define('PAYSTACK_SECRET_KEY', 'sk_test_11d4067841924afc5b15b8a3ffcf327862ea8f3b');
-
-define(
-    'PAYSTACK_BASE_URL',
-    'https://api.paystack.co'
-);
